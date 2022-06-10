2a3abd5f3c0561644fc1e55a7ccbbce487258bd1
Renzo <renzobelon@gmail.com>
2022-06-10 09:06:44 -0500

    first commit

---
+++ vibrating-heart/app.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container")
+const heartA = document.querySelector('.heart')
+// const heartB = document.querySelector('.heart::after')
+// const heartC = document.querySelector('.heart::before')
+
+function randomColor() {
+    const r = Math.ceil(Math.random() * 256) - 1;
+    const g = Math.ceil(Math.random() * 256) - 1;
+    const b = Math.ceil(Math.random() * 256) - 1;
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+setInterval(() => {
+
+},500)
+
+
